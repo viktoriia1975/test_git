97b07fb8a69b7ed654b0097761e9300f2075428e
viktoriia1975 <prudka33@gmail.com>
2020-08-06 13:58:08 -0400

    init

---
+++ src/lesson1/Main.java
@@ -0,0 +1,47 @@
+package lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        byte age;
+        age = 100;
+        System.out.println(age);
+
+        age = 5;
+        System.out.println(age);
+        age = 127;
+
+        short b;
+        b = 128;
+
+        int c;
+        c = 75;
+
+
+        long d;
+        d = 457;
+
+        float e;
+        e = 3.7f;
+
+        double f;
+        f = 7.5;
+
+        char k;
+        k = '$';
+
+        boolean m;
+        m = true;
+        m = false;
+        System.out.println(m);
+        // string
+        String z;
+        z = "My name is Vika";
+
+
+
+    }
+}
---
+++ src/lesson2/Main.java
@@ -0,0 +1,34 @@
+package lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    int age = 20;
+
+    String name = "John";
+
+
+//        if (age  != 20) {
+//            System.out.println("Welcome");
+//
+//        }
+//        else {
+//            System.out.println("Go home");
+//        }
+
+        if (name.equals("John") || age > 21){
+        System.out.println("Welcome");
+
+    } else{
+        System.out.println("Go home");
+
+    }
+
+    }
+}
+
+
+
